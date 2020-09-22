cc61a664fbc652f0b2ed16b8fd43adbf21a6e831
chul542 <chul542@yonsei.ac.kr>
2020-09-22 15:19:45 +0900

    Tip Calculator First Version

---
+++ tipCalculators.js
@@ -0,0 +1,9 @@
+console.log('How much aws your bill?');
+console.log("$ 350");
+console.log('How was your service?');
+console.log("30%");
+console.log('How many people are sharing the bill?');
+console.log('5 people');
+totalMoney = 350*1.3/5;
+console.log('calculate!');
+console.log('Each person need to pay ' + totalMoney + "$");
