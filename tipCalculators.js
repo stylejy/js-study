25fabc2e5a0976a79a28c56b75e906aca5ee0127
chul542 <chul542@yonsei.ac.kr>
2020-09-22 16:06:34 +0900

    Tip Calculator Seconed Version

--- tipCalculators.js
+++ tipCalculators.js
@@ -1,4 +1,4 @@
 function totalMoney(totalBill,serviceQual,numPeople){
     return totalBill*(1+serviceQual)/numPeople;
 };
-console.log(totalMoney(390,0.3,5)); //Total money that each person needs to pay
+console.log(totalMoney(390,0.3,5).toFixed(2)); //Total money that each person needs to pay
